9c57db9df957334f058a17431c793353e7f8c256
Junichi Ito <jit@sonicgarden.jp>
2024-05-03 20:32:49 +0900

    Tweak code for rubocop

--- lib/copy_tuner_incompatible_search/replace_command.rb
+++ lib/copy_tuner_incompatible_search/replace_command.rb
@@ -6,7 +6,7 @@ require 'set'
 require 'roo'
 
 module CopyTunerIncompatibleSearch
-  class ReplaceCommand
+  class ReplaceCommand # rubocop:disable Metrics/ClassLength
     def self.run(usage_path, blurbs_path, output_path)
       self.new(usage_path, blurbs_path).run(output_path)
     end
@@ -16,37 +16,38 @@ module CopyTunerIncompatibleSearch
       @blurbs_path = blurbs_path
     end
 
-    def run(output_path)
+    def run(output_path) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      # usages.xlsxから生成したUsageオブジェクトの配列
       usages = usage_sheet.each(type: 'Type', key: 'Key', ignored: 'Ignored', file: 'File', line: 'Line').filter_map.with_index do |hash, i|
         next if i.zero? # skip header
 
         Usage.new(**hash)
       end
+      # プロジェクト内で後方互換性のないキーの集合
       incompatible_keys = Set[*usages.select(&:static?).map(&:key)]
+      # 使用箇所が判明しているUsageオブジェクトの配列（ただし、dynamicなキーを除く）
       used_usages = usages.select(&:used?)
+      # _html付きのキーに変換する必要があるキーの集合
       keys_to_convert = Set[*used_usages.map(&:key)]
+      # staticかつ使用箇所が判明しているUsageオブジェクトの配列を翻訳キーでグループ化
       static_usages_by_key = used_usages.select(&:static?).group_by(&:key)
+      # lazyかつ使用箇所が判明しているUsageオブジェクトの配列を翻訳キーでグループ化
       lazy_usages_by_key = used_usages.select(&:lazy?).group_by(&:key)
 
-      keys_with_special_chars = []
-      all_blurb_keys = []
-      CSV.parse(blurbs_csv_text, headers: true, quote_char: '"').each do |row|
-        # FIXME: なぜか文字列で指定すると取得できない
-        # row['key']
-        key = row[0]
-        all_blurb_keys << key
-        translation = row[1]
-        if translation.match?(/&#\d+;|&\w+;/) && !key.match?(/[_.]html$/)
-          keys_with_special_chars << key
-        end
-      end
-      all_blurb_keys = Set[*all_blurb_keys]
-      dot_converted_keys = Set[*incompatible_keys.select { |k| all_blurb_keys.include?("#{k}.html") }]
+      # blurbs.csvの全キーの集合と、特殊文字を含むキーの配列
+      all_blurb_keys, keys_with_special_chars = parse_blurbs_csv
+      # すでに_htmlで終わるキーが存在する後方互換性のないキーの集合
       underscore_converted_keys = Set[*incompatible_keys.select { |k| all_blurb_keys.include?("#{k}_html") }]
+      # すでに.htmlで終わるキーが存在する後方互換性のないキーの集合
+      dot_converted_keys = Set[*incompatible_keys.select { |k| all_blurb_keys.include?("#{k}.html") }]
 
+      # コード上のキーを置換する必要があるキーの配列
       keys_for_code_replace = []
+      # _html付きのキーを新たに登録しなければならないキーの配列
       newly_replaced_keys = []
+      # 使用箇所が見つからなかった後方互換性のないキーの配列
       not_used_incompatible_keys = []
+      # _html付きのキーを定義したCSVを保存する。このファイルはCopyTunerにアップロードするために使う
       CSV.open(output_path, 'wb') do |csv_out|
         CSV.parse(blurbs_csv_text, headers: true, quote_char: '"').each_with_index do |row, i|
           if i.zero?
@@ -66,23 +67,8 @@ module CopyTunerIncompatibleSearch
         end
       end
 
-      # staticを置換
-      static_usages_by_key.each do |key, usages|
-        next unless keys_for_code_replace.include?(key)
-
-        usages.each do |usage|
-          replace_code(usage, all_blurb_keys)
-        end
-      end
-
-      # lazyを置換
-      lazy_usages_by_key.each do |key, usages|
-        next unless keys_for_code_replace.include?(key)
-
-        usages.each do |usage|
-          replace_code(usage, all_blurb_keys)
-        end
-      end
+      replace_code_for_static_usages(static_usages_by_key, keys_for_code_replace, all_blurb_keys)
+      replace_code_for_lazy_usages(lazy_usages_by_key, keys_for_code_replace, all_blurb_keys)
 
       existing_keys = keys_for_code_replace - newly_replaced_keys
       already_ignored_keys = search_ignored_keys.uniq
@@ -132,7 +118,7 @@ module CopyTunerIncompatibleSearch
     class Result
       attr_reader :newly_replaced_keys, :existing_keys, :not_used_incompatible_keys, :keys_to_ignore, :already_ignored_keys, :keys_with_special_chars
 
-      def initialize(newly_replaced_keys, existing_keys, not_used_incompatible_keys, keys_to_ignore, already_ignored_keys, keys_with_special_chars)
+      def initialize(newly_replaced_keys, existing_keys, not_used_incompatible_keys, keys_to_ignore, already_ignored_keys, keys_with_special_chars) # rubocop:disable Metrics/ParameterLists
         @newly_replaced_keys = newly_replaced_keys
         @existing_keys = existing_keys
         @not_used_incompatible_keys = not_used_incompatible_keys
@@ -157,6 +143,42 @@ module CopyTunerIncompatibleSearch
 
     def ignored_keys_text
       `rails r "p CopyTunerClient::configuration.ignored_keys"`
+    end
+
+    def parse_blurbs_csv
+      keys_with_special_chars = []
+      all_blurb_keys = []
+      CSV.parse(blurbs_csv_text, headers: true, quote_char: '"').each do |row|
+        # FIXME: なぜか文字列で指定すると取得できない
+        # row['key']
+        key = row[0]
+        all_blurb_keys << key
+        translation = row[1]
+        if translation.match?(/&#\d+;|&\w+;/) && !key.match?(/[_.]html$/)
+          keys_with_special_chars << key
+        end
+      end
+      [Set[*all_blurb_keys], keys_with_special_chars]
+    end
+
+    def replace_code_for_lazy_usages(lazy_usages_by_key, keys_for_code_replace, all_blurb_keys)
+      lazy_usages_by_key.each do |key, usages|
+        next unless keys_for_code_replace.include?(key)
+
+        usages.each do |usage|
+          replace_code(usage, all_blurb_keys)
+        end
+      end
+    end
+
+    def replace_code_for_static_usages(static_usages_by_key, keys_for_code_replace, all_blurb_keys)
+      static_usages_by_key.each do |key, usages|
+        next unless keys_for_code_replace.include?(key)
+
+        usages.each do |usage|
+          replace_code(usage, all_blurb_keys)
+        end
+      end
     end
 
     def replace_code(usage, all_blurb_keys)
