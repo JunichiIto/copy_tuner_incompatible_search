26e416a33417e77dbff8ca2b2daf74981284a92c
Junichi Ito <jit@sonicgarden.jp>
2024-05-03 20:52:32 +0900

    Add specs

--- spec/copy_tuner_incompatible_search/replace_command_spec.rb
+++ spec/copy_tuner_incompatible_search/replace_command_spec.rb
@@ -178,6 +178,110 @@ RSpec.describe CopyTunerIncompatibleSearch::ReplaceCommand, :aggregate_failures 
           assert_csv(output_path, expected_csv)
         end
       end
+
+      context 'すでにコードを_htmlで終わるキーに置換している場合' do
+        let(:blurbs_csv_text) do
+          <<~CSV
+            key,ja,created_at,ja updated_at,ja updater
+            sample.hello,"Hello,<br/>world!",2013/05/28 10:51:09,2013/05/28 10:51:11,
+            sample.hello_html,"Hello,<br/>world!",2013/05/28 10:51:09,2013/05/28 10:51:11,
+          CSV
+        end
+        let(:usage_data) do
+          [
+            { type: 'Type', key: 'Key', ignored: 'Ignored', file: 'File', line: 'Line' },
+            { type: 'static', key: 'sample.hello', ignored: 'N', file: 'app/views/home/index.html.haml', line: 2 },
+          ]
+        end
+
+        before do
+          sheet_mock = double('sheet')
+          allow(sheet_mock).to receive(:each).and_return(usage_data)
+          allow(command).to receive(:usage_sheet).and_return(sheet_mock)
+          allow(command).to receive(:blurbs_csv_text).and_return(blurbs_csv_text)
+          allow(command).to receive(:file_readlines).and_return(<<~HAML.lines)
+            %h1 Welcome
+            %p= t('sample.hello_html')
+            %h2 Contents
+          HAML
+          allow(command).to receive(:ignored_keys_text).and_return('[]')
+        end
+
+        it '二重に置換しない / CSVにも出力されない' do
+          expect(command).to receive(:file_write).with('app/views/home/index.html.haml', <<~HAML)
+            %h1 Welcome
+            %p= t('sample.hello_html')
+            %h2 Contents
+          HAML
+          expect do
+            result = command.run(output_path)
+            expect(result.newly_replaced_keys).to eq []
+            expect(result.existing_keys).to eq ['sample.hello']
+            expect(result.not_used_incompatible_keys).to eq []
+            expect(result.keys_to_ignore).to eq ['sample.hello']
+            expect(result.already_ignored_keys).to eq []
+            expect(result.keys_with_special_chars).to eq []
+            expect(result.dynamic_count).to eq 0
+          end.to change { File.exist?(output_path) }.from(false)
+
+          expected_csv = <<~CSV
+            key,ja,created_at,ja updated_at,ja updater
+          CSV
+          assert_csv(output_path, expected_csv)
+        end
+      end
+
+      context 'すでにコードを.htmlで終わるキーに置換している場合' do
+        let(:blurbs_csv_text) do
+          <<~CSV
+            key,ja,created_at,ja updated_at,ja updater
+            sample.hello,"Hello,<br/>world!",2013/05/28 10:51:09,2013/05/28 10:51:11,
+            sample.hello.html,"Hello,<br/>world!",2013/05/28 10:51:09,2013/05/28 10:51:11,
+          CSV
+        end
+        let(:usage_data) do
+          [
+            { type: 'Type', key: 'Key', ignored: 'Ignored', file: 'File', line: 'Line' },
+            { type: 'static', key: 'sample.hello', ignored: 'N', file: 'app/views/home/index.html.haml', line: 2 },
+          ]
+        end
+
+        before do
+          sheet_mock = double('sheet')
+          allow(sheet_mock).to receive(:each).and_return(usage_data)
+          allow(command).to receive(:usage_sheet).and_return(sheet_mock)
+          allow(command).to receive(:blurbs_csv_text).and_return(blurbs_csv_text)
+          allow(command).to receive(:file_readlines).and_return(<<~HAML.lines)
+            %h1 Welcome
+            %p= t('sample.hello.html')
+            %h2 Contents
+          HAML
+          allow(command).to receive(:ignored_keys_text).and_return('[]')
+        end
+
+        it '二重に置換しない / CSVにも出力されない' do
+          expect(command).to receive(:file_write).with('app/views/home/index.html.haml', <<~HAML)
+            %h1 Welcome
+            %p= t('sample.hello.html')
+            %h2 Contents
+          HAML
+          expect do
+            result = command.run(output_path)
+            expect(result.newly_replaced_keys).to eq []
+            expect(result.existing_keys).to eq ['sample.hello']
+            expect(result.not_used_incompatible_keys).to eq []
+            expect(result.keys_to_ignore).to eq ['sample.hello']
+            expect(result.already_ignored_keys).to eq []
+            expect(result.keys_with_special_chars).to eq []
+            expect(result.dynamic_count).to eq 0
+          end.to change { File.exist?(output_path) }.from(false)
+
+          expected_csv = <<~CSV
+            key,ja,created_at,ja updated_at,ja updater
+          CSV
+          assert_csv(output_path, expected_csv)
+        end
+      end
     end
 
     context 'lazyなキーを置換する場合' do
